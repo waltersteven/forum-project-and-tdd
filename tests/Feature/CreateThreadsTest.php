2306e0125cb6fb00657b750643006bc4858f318e
Walter Pariona Sánchez <wltrst97@gmail.com>
2019-11-09 16:16:27 -0500

    Added validation methods and tests

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -11,6 +11,18 @@ class CreateThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    /** @test */
+    function guest_may_not_create_threads()
+    {
+        //When visiting the page, guest may be redirected
+        $this->get('/threads/create')
+            ->assertRedirect('/login');
+
+        //When hitting the endpoint, guest may be redirected
+        $this->post('/threads')
+            ->assertRedirect('/login');
+    }
+
     /** @test  */
     function an_authenticated_user_can_create_new_forum_threads()
     {
@@ -22,24 +34,46 @@ class CreateThreadsTest extends TestCase
 
         // $thread = factory('App\Thread')->raw(); //returns an array
 
-        $thread = create('App\Thread'); //returns an instance
+        $thread = make('App\Thread'); //returns an array
 
-        $this->post('/threads', $thread->toArray());
+        $response = $this->post('/threads', $thread->toArray());
 
         //Then, when we visit the thread page, We should see the new Thread
-        $this->get($thread->path())
+        $this->get($response->headers->get('Location'))
             ->assertSee($thread->title)
             ->assertSee($thread->body);
     }
-    /** @test */
-    function guest_may_not_create_threads()
-    {
-        //When visiting the page, guest may be redirected
-        $this->get('/threads/create')
-            ->assertRedirect('/login');
 
-        //When hitting the endpoint, guest may be redirected
-        $this->post('/threads')
-            ->assertRedirect('/login');
+    /** @test */
+    function a_thread_requires_a_title()
+    {
+        $this->publishThread(['title' => null])
+            ->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    function a_thread_requires_a_body()
+    {
+        $this->publishThread(['body' => null])
+            ->assertSessionHasErrors('body');
+    }
+
+    /** @test */
+    function a_thread_requires_a_valid_channel()
+    {
+        $this->publishThread(['channel_id' => null])
+            ->assertSessionHasErrors('channel_id');
+
+        $this->publishThread(['channel_id' => 999])
+            ->assertSessionHasErrors('channel_id');
+    }
+
+    public function publishThread($overrides)
+    {
+        $this->signIn();
+
+        $thread = make('App\Thread', $overrides);
+
+        return $this->post('/threads', $thread->toArray());
     }
 }

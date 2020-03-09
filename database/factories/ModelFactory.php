2d70f34970261817501a1e3d84d67db663903d2b
Walter Pariona <wltrst97@gmail.com>
2020-03-09 00:29:58 -0500

    Added modelFactory for Notifications

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -4,7 +4,6 @@
 
 use App\Model;
 use Faker\Generator as Faker;
-use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,8 +21,8 @@ $factory->define(App\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+        'remember_token' => Illuminate\Support\Str::random(10),
     ];
 });
 
@@ -57,5 +56,21 @@ $factory->define(App\Reply::class, function (Faker $faker) {
             return factory('App\User')->create()->id;
         },
         'body' => $faker->paragraph
+    ];
+});
+
+$factory->define(Illuminate\Notifications\DatabaseNotification::class, function (Faker $faker) {
+    return [
+        'id' => Illuminate\Support\Str::uuid()->toString(),
+
+        'type' => 'App\Notifications\ThreadWasUpdated',
+
+        'notifiable_id' => function () {
+            return auth()->id() ?: factory('App\User')->create()->id;
+        },
+
+        'notifiable_type' => 'App\User',
+
+        'data' => ['foo' => 'bar']
     ];
 });
--- tests/Feature/NotificationsTest.php
+++ tests/Feature/NotificationsTest.php
@@ -5,17 +5,22 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Notifications\DatabaseNotification;
 use Tests\TestCase;
 
 class NotificationsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function setUp() : void
+    {
+        parent::setUp();
+        $this->signIn();
+    }
+
     /** @test */
     public function a_notification_is_prepared_when_a_subscribed_thread_receives_a_new_reply_that_is_not_by_the_current_user()
     {
-        $this->signIn();
-
         $thread = create('App\Thread')->subscribe();
 
         $this->assertCount(0, auth()->user()->notifications);
@@ -42,44 +47,33 @@ class NotificationsTest extends TestCase
     /** @test */
     public function a_user_can_fetch_their_unread_notifications()
     {
-        $this->signIn();
+        // Before creating a Notification Factory
 
-        $thread = create('App\Thread')->subscribe();
+        // $thread = create('App\Thread')->subscribe();
+        // $thread->addReply(['user_id' => create('App\User')->id, 'body' => 'Some reply here']);
 
-        $thread->addReply([
-            'user_id' => create('App\User')->id,
-            'body' => 'Some reply here'
-        ]);
+        // After creating a Notification Factory
 
-        $user = auth()->user();
+        // create('Illuminate\Notifications\DatabaseNotification'); // or
+        create(DatabaseNotification::class);
 
-        $response = $this->getJson("/profiles/{$user->name}/notifications")->json();
-
-        $this->assertCount(1, $response);
+        $this->assertCount(
+            1,
+            $this->getJson('/profiles/' . auth()->user()->name . '/notifications')->json()
+        );
     }
 
     /** @test */
     public function a_user_can_mark_a_notification_as_read()
     {
-        $this->withoutExceptionHandling();
+        create(DatabaseNotification::class);
 
-        $this->signIn();
+        tap(auth()->user(), function ($user) {
+            $this->assertCount(1, $user->unreadNotifications);
 
-        $thread = create('App\Thread')->subscribe();
+            $this->delete("/profiles/{$user->name}/notifications/" . $user->unreadNotifications->first()->id);
 
-        $thread->addReply([
-            'user_id' => create('App\User')->id,
-            'body' => 'Some reply here'
-        ]);
-
-        $user = auth()->user();
-
-        $this->assertCount(1, $user->unreadNotifications);
-
-        $notificationId = $user->unreadNotifications->first()->id;
-
-        $this->delete("/profiles/{$user->name}/notifications/{$notificationId}");
-
-        $this->assertCount(0, $user->fresh()->unreadNotifications);
+            $this->assertCount(0, $user->fresh()->unreadNotifications);
+        });
     }
 }

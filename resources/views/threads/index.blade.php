b8908413bc22c17ad963fafacba336465188f971
Walter Pariona <wltrst97@gmail.com>
2020-04-12 19:40:38 -0500

    Some tweaks in views

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,33 +4,9 @@
 <div class="container">
 	<div class="row justify-content-center">
 		<div class="col-md-8">
-			@forelse ($threads as $thread)
-			<div class="card mt-4">
-				<div class="card-header">
-					<div class="level">
-						<h4 class="flex">
-							<a href="{{ $thread->path() }}">
-								@if (auth()->check() && $thread->hasUpdatesFor(auth()->user()))
-								<strong>
-									{{ $thread->title }}
-								</strong>
-								@else
-								{{ $thread->title }}
-								@endif
-							</a>
-						</h4>
-						<a href="{{ $thread->path() }}">
-							{{ $thread->replies_count }} {{ Str::plural('reply', $thread->replies_count) }}
-						</a>
-					</div>
-				</div>
-				<div class="card-body">
-					<div class="body">{{ $thread->body }}</div>
-				</div>
-			</div>
-			@empty
-			<p>There are no relevant results at this time.</p>
-			@endforelse
+			@include('threads._list')
+
+			{{ $threads->render() }}
 		</div>
 	</div>
 </div>

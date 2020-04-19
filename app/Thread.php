8430f10f3937e263d074ec95eacc5358d8bbe79e
Walter Pariona <wltrst97@gmail.com>
2020-04-18 21:57:45 -0500

    Changed Trait to Class

--- app/Thread.php
+++ app/Thread.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    use RecordsActivity, RecordsVisits;
+    use RecordsActivity;
 
     protected $guarded = [];
 
@@ -135,5 +135,10 @@ class Thread extends Model
         $key = $user->visitedThreadCacheKey($this);
 
         return $this->updated_at > cache($key);
+    }
+
+    public function visits()
+    {
+        return new Visits($this);
     }
 }
